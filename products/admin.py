d97fd1da22ef2465ec484c21b1478a3e5934d3b1
Gabi Nagy <gabrian.nagy@gmail.com>
2020-07-13 20:59:13 +0300

    Add Product, Batch models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from utils.admin import BaseModelAdmin
+from .models import Product, Batch
+
+
+class ProductAdmin(BaseModelAdmin):
+    list_display = ['id', 'created_on', 'updated_on', 'name']
+    readonly_fields = ['id', 'created_on', 'updated_on']
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class BatchAdmin(BaseModelAdmin):
+    list_display = ['id', 'created_on', 'updated_on', 'product', 'amount', 'expires_on']
+    readonly_fields = ['id', 'created_on', 'updated_on']
+    autocomplete_fields = ['product']
+
+
+admin.site.register(Batch, BatchAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
+from utils.models import BaseModel
 from django.db import models
 
-# Create your models here.
+
+class Product(BaseModel):
+    name = models.CharField(max_length=64)
+    description = models.TextField(max_length=1024, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Batch(BaseModel):
+
+    # models.CASCADE: Django also deletes the object containing the ForeignKey
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    expires_on = models.DateField()
+
+    def __str__(self):
+        return '{} ({})'.format(self.product.name, self.expires_on)
+
+    class Meta:
+        verbose_name_plural = 'Batches'
