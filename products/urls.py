eda1169f0a2882a883d4db7221e758f95847e44e
Gabi Nagy <gabrian.nagy@gmail.com>
2020-07-14 06:30:00 +0300

    Add product_inventory, product_inventory_overview views

--- products/urls.py
+++ products/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
 
     path('batches/', views.BatchList.as_view(), name='batch_list'),
     path('batches/<int:pk>/', views.BatchDetail.as_view(), name='batch_detail'),
+
+    path('products/<int:pk>/inventory/', views.product_inventory, name='product_inventory'),
+    path('products/<int:pk>/inventory_overview/', views.product_inventory_overview, name='product_inventory_overview'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- products/views.py
+++ products/views.py
@@ -1,4 +1,9 @@
-from rest_framework import generics
+import datetime
+
+from django.db.models import Sum
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Product, Batch
 from .serializers import ProductSerializer, BatchSerializer
@@ -22,3 +27,39 @@ class BatchList(generics.ListCreateAPIView):
 class BatchDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Batch.objects.all()
     serializer_class = BatchSerializer
+
+
+@api_view(['GET'])
+def product_inventory(request, pk):
+    """
+    Retrieve product inventory information.
+    """
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        batches = Batch.objects.filter(product=product).order_by('expires_on').all()
+        serializer = BatchSerializer(batches, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+def product_inventory_overview(request, pk):
+    """
+    Retrieve product inventory overview information.
+    """
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        today = datetime.date.today()
+        amounts = {
+            'fresh': Batch.objects.filter(product=product, expires_on__gt=today).aggregate(Sum('amount'))['amount__sum'] or 0,
+            'expiring_today': Batch.objects.filter(product=product, expires_on=today).aggregate(Sum('amount'))['amount__sum'] or 0,
+            'expired': Batch.objects.filter(product=product, expires_on__lt=today).aggregate(Sum('amount'))['amount__sum'] or 0,
+        }
+        return Response(amounts)
