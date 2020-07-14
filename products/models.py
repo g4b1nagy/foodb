5e688ca3f7f82c408f9b0c98066a51998f84d84f
Gabi Nagy <gabrian.nagy@gmail.com>
2020-07-14 05:43:13 +0300

    Add commas at end of comments

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Product(BaseModel):
 
     # Note that when unique is True, you don’t need to specify db_index,
-    # because unique implies the creation of an index
+    # because unique implies the creation of an index.
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(max_length=1024, blank=True)
 
@@ -15,7 +15,7 @@ class Product(BaseModel):
 
 class Batch(BaseModel):
 
-    # models.CASCADE: Django also deletes the object containing the ForeignKey
+    # models.CASCADE: Django also deletes the object containing the ForeignKey.
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     amount = models.IntegerField()
     expires_on = models.DateField()
